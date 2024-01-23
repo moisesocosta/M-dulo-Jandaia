ee5da8fd681de5cc3b6b1449238c2e9843773e2e
Moisés Oliveira Costa <mosolicos@gmail.com>
2024-01-23 15:30:11 -0300

    Aula 1.2
    Desenvolvimento de uma API de produtos , usando Maven, Spring, Cucumber e JUnit

---
+++ livecodebdd/src/main/java/com/livecodebdd/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.livecodebdd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @GetMapping("/products")
+    public List<String> getAllProducts(){
+        List<String> products = new ArrayList<String>();
+
+        products.add("Mouse");
+        products.add("Keyboard");
+        products.add("CellPhone");
+        products.add("Screen");
+        products.add("Notebook");
+        products.add("TV");
+
+        return products;
+    }
+}
